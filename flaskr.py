bf0ce9e027c5aaaa7e551faaf0e1af8621749f32
krconrad <chocokrizx@gmail.com>
2015-04-01 12:56:31 -0600

    Add user recognition to logged in page.

--- flaskr.py
+++ flaskr.py
@@ -2,10 +2,14 @@
 
 import sqlite3
 
+from datetime import datetime, timedelta
 from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+     abort, render_template, flash, jsonify
+from flask import session, request
 from contextlib import closing
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import gen_salt
+from flask_oauthlib.provider import OAuth2Provider
 
 # configuration
 DATABASE = './flaskr.db' #create database in local directory
@@ -17,17 +21,20 @@ PASSWORD = 'default'
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.debug = True
+app.secret_key = 'secret'
 app.config.update({
     'SQLALCHEMY_DATABASE_URI': 'sqlite:///flaskr.db',
 })
 db = SQLAlchemy(app)
+oauth = OAuth2Provider(app)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), unique=True)
     password = db.Column(db.String(20))
-    # __init__ is an initializer, makes new instance of a model, constructor for a class. ex: User(josie, dog)
+    # __init__ is an initializer, makes new instance of a model, constructor for an object from a class. ex: User(josie, dog)
     def __init__(self, username, password): 
         self.username = username
         self.password = password
@@ -130,17 +137,134 @@ class Token(db.Model):
             return self._scopes.split()
         return []
 
-#need to have current user to let system know that this user is currently logged in
+#looks up the user id in the session and finds the user in the database. 
 def current_user():
     if 'id' in session:
         uid = session['id']
         return User.query.get(uid)
     return None
 
-@app.route('/')
+@app.route('/client')
+def client():
+    user = current_user()
+    if not user:
+        return redirect('/')
+    item = Client(
+        client_id=gen_salt(40),
+        client_secret=gen_salt(50),
+        _redirect_uris=' '.join([
+            'http://localhost:8000/authorized',
+            'http://127.0.0.1:8000/authorized',
+            'http://127.0.1:8000/authorized',
+            'http://127.1:8000/authorized',
+            ]),
+        _default_scopes='email',
+        user_id=user.id,
+    )
+    db.session.add(item)
+    db.session.commit()
+    return jsonify(
+        client_id=item.client_id,
+        client_secret=item.client_secret,
+    )
+
+@oauth.clientgetter
+def load_client(client_id):
+    return Client.query.filter_by(client_id=client_id).first()
+
+
+@oauth.grantgetter
+def load_grant(client_id, code):
+    return Grant.query.filter_by(client_id=client_id, code=code).first()
+
+
+@oauth.grantsetter
+def save_grant(client_id, code, request, *args, **kwargs):
+    # decide the expires time yourself
+    expires = datetime.utcnow() + timedelta(seconds=100)
+    grant = Grant(
+        client_id=client_id,
+        code=code['code'],
+        redirect_uri=request.redirect_uri,
+        _scopes=' '.join(request.scopes),
+        user=current_user(),
+        expires=expires
+    )
+    db.session.add(grant)
+    db.session.commit()
+    return grant
+
+
+@oauth.tokengetter
+def load_token(access_token=None, refresh_token=None):
+    if access_token:
+        return Token.query.filter_by(access_token=access_token).first()
+    elif refresh_token:
+        return Token.query.filter_by(refresh_token=refresh_token).first()
+
+
+@oauth.tokensetter
+def save_token(token, request, *args, **kwargs):
+    print "request: " + str(request)
+    print "token: " + str(token)
+    toks = Token.query.filter_by(
+        client_id=request.client.client_id,
+        user_id=request.user.id
+    )
+    # make sure that every client has only one token connected to a user
+    for t in toks:
+        db.session.delete(t)
+
+    expires_in = token.pop('expires_in')
+    expires = datetime.utcnow() + timedelta(seconds=expires_in)
+
+    tok = Token(
+        access_token=token['access_token'],
+        refresh_token=token['refresh_token'],
+        token_type=token['token_type'],
+        _scopes=token['scope'],
+        expires=expires,
+        client_id=request.client.client_id,
+        user_id=request.user.id,
+    )
+    db.session.add(tok)
+    db.session.commit()
+    return tok
+
+
+@app.route('/oauth/token', methods=['GET', 'POST'])
+@oauth.token_handler
+def access_token():
+    return None
+
+
+@app.route('/oauth/authorize', methods=['GET', 'POST'])
+@oauth.authorize_handler
+def authorize(*args, **kwargs):
+    user = current_user()
+    if not user:
+        return redirect('/')
+    if request.method == 'GET':
+        client_id = kwargs.get('client_id')
+        client = Client.query.filter_by(client_id=client_id).first()
+        kwargs['client'] = client
+        kwargs['user'] = user
+        return render_template('authorize.html', **kwargs)
+
+    confirm = request.form.get('confirm', 'no')
+    return confirm == 'yes'
+
+
+@app.route('/api/me')
+@oauth.require_oauth()
+def me():
+    user = request.oauth.user
+    return jsonify(username=user.username)
+
+@app.route('/') #look at this after integrating code, but this is home
 def show_entries():
-    entries = Entry.query.all()
-    return render_template('show_entries.html', entries=entries)
+    entries = Entry.query.all() #asks database for all of the entries. Then assigns it to local variable: entries.
+    return render_template('show_entries.html', entries=entries, user=current_user())
 
 @app.route('/add', methods=['POST'])
 def add_entry():
@@ -163,7 +287,9 @@ def login():
             error = 'Invalid password'
         else:
             session['logged_in'] = True
-            flash('You were logged in')
+            #stores the user in session. 
+            session['id'] = user.id 
+            flash('You were logged in!')
             return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
 
@@ -188,4 +314,5 @@ def signup():
     return render_template('signup.html')
 
 if __name__ == '__main__':
+    db.create_all()
     app.run()
